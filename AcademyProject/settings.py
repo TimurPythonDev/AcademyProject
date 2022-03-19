3c42af6e192cd044d01dff393e726191876a4943
TimurPythonDev <karabayevtimurpython@gmail.com>
2022-03-19 21:59:59 +0300

    heroku static1

--- AcademyProject/settings.py
+++ AcademyProject/settings.py
@@ -123,7 +123,7 @@ STATIC_URL = 'static/'
 STATIC_ROOT = '/static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR,'static')
+    os.path.join(BASE_DIR,'static'),
 ]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
